adf9b7dc67bd054d6a8f03796c72eae7f6312bdd
villarealalexander <villarealat.675.stud@cdd.edu.ph>
2024-05-30 10:04:26 +0800

    fix

--- resources/views/layouts/master-layout.blade.php
+++ resources/views/layouts/master-layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title') - UdD Registrar Records Repository Management System</title>
+    <title>@yield('title') - UdD Registrar Repository Management System</title>
     <!-- Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <!-- Font Awesome -->
